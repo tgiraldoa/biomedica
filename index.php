6f6ed04c2c502ab9da54d15e0617dc5fa62209eb
Tomas Giraldo Agudelo <tomasga98@hotmail.com>
2020-07-07 12:14:52 -0500

    Agrego el primer modelo

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+	<title>BIOMEDICA ESTETICA | HOME</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+	<script src="vista/js/jquery.min.js" type="text/javascript"></script>
+
+	<link rel="stylesheet" href="vista/css/cabezote.css">
+</head>
+<body>
+	<?php 
+	
+	//INCLUIMOS EL MODULO DEL CABEZOTE DONDE TENEMOS LAS OPCIONES DEL MENU
+	include 'vista/modulos/cabezote.html';
+
+	//INCLUIMOS LAS TABLAS PRINCIPALES PARA LA VISTA DE RESUMEN DEL DIA
+	include 'vista/modulos/body.html';
+
+	?>
+</body>
+</html>
